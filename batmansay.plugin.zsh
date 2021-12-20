1d65cfdf844b4468f1d313128edd10d3ec310350
Hank Lin <59768545+FullstackGJJ@users.noreply.github.com>
2021-12-20 01:14:52 -0500

    Update batmansay.plugin.zsh

--- batmansay.plugin.zsh
+++ batmansay.plugin.zsh
@@ -1,7 +1,7 @@
 ## Welcome to use DOGESAY zsh plugin!
 
 ## words that doge say
-declare -a WOWARR=("BATMAN SAYS:")
+declare -a WOWARR=("BATMAN SAW:")
 WOWARRLEN=${#WOWARR[@]}
 
 ## A list of element that doge should not say a word about
